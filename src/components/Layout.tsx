1cbeb356c6f5c79efc5e6c8792332c92f253adab
Sebs <seva32@yahoo.es>
2025-03-14 16:06:04 -0400

    update navbar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,10 +4,7 @@ import NavBar from './NavBar'
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
-      <header className="p-4 bg-blue-600 text-white text-xl font-bold">
-        The Mars Project
-        <NavBar />
-      </header>
+      <NavBar />
       <main className="flex-1 p-4">{children}</main>
       <footer className="p-4 bg-gray-800 text-white text-center">
         © 2025 Mars Project
