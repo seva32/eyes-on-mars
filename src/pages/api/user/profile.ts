aecba79cac0a09dedee17e6833365bdcc72a34e3
Sebs <sebastian.fantini@livestories.com>
2025-03-18 15:17:57 -0400

    fix profile init flow

--- src/pages/api/user/profile.ts
+++ src/pages/api/user/profile.ts
@@ -1,26 +1,28 @@
 import { NextApiResponse } from 'next'
-import { authMiddleware } from '../../../middleware/auth'
+import { getToken, JWT } from 'next-auth/jwt'
 import prisma from '../../../lib/prisma'
 
 const handler = async (req: AuthenticatedRequest, res: NextApiResponse) => {
   if (req.method === 'GET') {
-    const user = req.user
-    console.log('User: >>>>>>>>>>//////////', user)
-    if (!user) {
+    const token: JWT & { user?: { id: number } } = await getToken({ req })
+    if (!token) {
       return res.status(401).json({ message: 'Unauthorized' })
     }
 
+    const userId = token.user?.id || parseInt(token.sub, 10)
+
+    if (!userId) {
+      return res.status(400).json({ message: 'Invalid user data' })
+    }
+
     const profile = await prisma.user.findUnique({
-      where: { id: user.id },
-      include: { profile: true, favoritePhotos: true },
+      where: { id: userId },
+      include: { profile: true, favorite_photo: true },
     })
 
-    console.log('antes: >>>>>>>>>>//////////')
     if (!profile) {
       return res.status(404).json({ message: 'Profile not found' })
     }
-
-    console.log('despues: >>>>>>>>>>//////////')
 
     return res.status(200).json(profile)
   }
@@ -28,4 +30,4 @@ const handler = async (req: AuthenticatedRequest, res: NextApiResponse) => {
   res.status(405).json({ message: 'Method not allowed' })
 }
 
-export default authMiddleware(handler)
+export default handler
