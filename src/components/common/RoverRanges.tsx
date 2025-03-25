c00ba9718fd419c0087ffe56e4dd1c0fad0a96e4
Sebs <seva32@yahoo.es>
2025-03-24 21:46:42 -0400

    filter redesign

---
+++ src/components/common/RoverRanges.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const data = [
+  {
+    rover: 'Curiosity',
+    landing: 'August 6, 2012',
+    solRange: '0 to Present (still active)',
+  },
+  {
+    rover: 'Spirit',
+    landing: 'January 4, 2004',
+    solRange: '0 to 2208 (last contact in 2010)',
+  },
+  {
+    rover: 'Opportunity',
+    landing: 'January 25, 2004',
+    solRange: '0 to 5111 (last contact in 2018)',
+  },
+]
+
+const MarsRoverChart = () => {
+  return (
+    <div className="w-full max-w-2xl mx-auto font-sans p-4">
+      <h2 className="text-center text-xl font-bold mb-4">
+        Mars Rover Mission Timelines
+      </h2>
+      <div className="overflow-x-auto rounded-md">
+        <table className="w-full text-left text-black">
+          <thead>
+            <tr className="bg-red-300">
+              <th className="p-2">Rover</th>
+              <th className="p-2">Landing Date (Earth)</th>
+              <th className="p-2">Sol Range</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((rover) => (
+              <tr key={rover.rover} className="odd:bg-red-100 even:bg-red-100">
+                <td className="p-2 font-semibold">{rover.rover}</td>
+                <td className="p-2">{rover.landing}</td>
+                <td className="p-2">{rover.solRange}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
+
+export default MarsRoverChart
--- src/components/common/SearchControls.tsx
+++ src/components/common/SearchControls.tsx
@@ -1,5 +1,6 @@
 'use client'
 import * as React from 'react'
+import { useState } from 'react'
 import { MdClear } from 'react-icons/md'
 
 interface SearchControlsProps {
@@ -19,11 +20,16 @@ export function SearchControls({
   onSearch,
   isDisabled,
 }: SearchControlsProps) {
+  const [activeTab, setActiveTab] = useState<'earthDate' | 'solDay'>(
+    'earthDate',
+  )
+
   const handleChangeEarthDate = (e: React.ChangeEvent<HTMLInputElement>) => {
     try {
       const date = new Date(e.target.value)
       const formattedDate = date.toISOString().split('T')[0] // Format date as YYYY-MM-DD
       setEarthDate(formattedDate)
+      setSolDay('') // Clear Sol Day when Earth Date is set
     } catch {}
   }
 
@@ -36,60 +42,96 @@ export function SearchControls({
   }
 
   return (
-    <section className="flex gap-4 mb-8 max-sm:flex-col">
-      <div className="flex-1 relative">
-        <label htmlFor="earth-date" className="mb-2 text-sm">
-          Earth Date
-        </label>
-        <input
-          id="earth-date"
-          type="date"
-          className="p-3 w-full rounded-lg border border-solid bg-zinc-900 border-zinc-800 text-zinc-200"
-          value={earthDate}
-          onChange={handleChangeEarthDate}
-        />
-        {earthDate && (
-          <button
-            type="button"
-            className="absolute right-10 top-10 text-zinc-200 cursor-pointer"
-            onClick={handleClearEarthDate}
-          >
-            <MdClear />
-          </button>
-        )}
-      </div>
-      <div className="flex-1 relative">
-        <label htmlFor="sol-day" className="mb-2 text-sm">
-          Sol Day
-        </label>
-        <input
-          id="sol-day"
-          type="number"
-          placeholder="Enter Sol day"
-          className="p-3 w-full rounded-lg border border-solid bg-zinc-900 border-zinc-800 text-zinc-200"
-          value={solDay}
-          onChange={(e) => setSolDay(e.target.value)}
-        />
-        {solDay && (
-          <button
-            type="button"
-            className="absolute right-10 top-10 text-zinc-200 cursor-pointer"
-            onClick={handleClearSolDay}
-          >
-            <MdClear />
-          </button>
-        )}
-      </div>
-      <div className="flex items-end">
+    <section className="flex flex-col gap-4 mb-8 w-96 m-auto">
+      <div className="flex gap-1">
         <button
-          className="px-4 py-3 font-semibold text-white bg-red-500 rounded-lg cursor-pointer"
-          onClick={onSearch}
-          style={{ opacity: isDisabled ? '0.5' : '1' }}
-          disabled={isDisabled}
+          className={`py-0.5 font-semibold rounded-md flex-1 border-red-500 border-4 cursor-pointer ${
+            activeTab === 'earthDate'
+              ? 'bg-red-500 text-white'
+              : 'bg-red-100 text-red-400'
+          }`}
+          onClick={() => {
+            handleClearSolDay()
+            setActiveTab('earthDate')
+          }}
         >
-          Search Photos
+          Earth Date
+        </button>
+        <button
+          className={`py-0.5 font-semibold rounded-md flex-1 border-red-500 border-4 cursor-pointer ${
+            activeTab === 'solDay'
+              ? 'bg-red-500 text-white'
+              : 'bg-red-100 text-red-400'
+          }`}
+          onClick={() => {
+            handleClearEarthDate()
+            setActiveTab('solDay')
+          }}
+        >
+          Sol Day
         </button>
       </div>
+
+      {activeTab === 'earthDate' && (
+        <div className="flex-1 relative">
+          <label htmlFor="earth-date" className="mb-2 text-sm">
+            Earth Date
+          </label>
+          <input
+            id="earth-date"
+            type="date"
+            className="p-3 w-full rounded-lg border border-solid bg-zinc-900 border-zinc-800 text-zinc-200"
+            value={earthDate}
+            onChange={handleChangeEarthDate}
+          />
+          {earthDate && (
+            <button
+              type="button"
+              className="absolute right-10 top-10 text-zinc-200 cursor-pointer"
+              onClick={handleClearEarthDate}
+            >
+              <MdClear />
+            </button>
+          )}
+        </div>
+      )}
+
+      {activeTab === 'solDay' && (
+        <div className="flex-1 relative">
+          <label htmlFor="sol-day" className="mb-2 text-sm">
+            Sol Day
+          </label>
+          <input
+            id="sol-day"
+            type="number"
+            placeholder="Enter Sol day"
+            className="p-3 w-full rounded-lg border border-solid bg-zinc-900 border-zinc-800 text-zinc-200"
+            value={solDay}
+            onChange={(e) => {
+              setSolDay(e.target.value)
+              setEarthDate('') // Clear Earth Date when Sol Day is set
+            }}
+          />
+          {solDay && (
+            <button
+              type="button"
+              className="absolute right-10 top-10 text-zinc-200 cursor-pointer"
+              onClick={handleClearSolDay}
+            >
+              <MdClear />
+            </button>
+          )}
+        </div>
+      )}
+
+      <button
+        className="py-2 font-semibold text-white bg-red-500 rounded-lg cursor-pointer"
+        onClick={onSearch}
+        style={{ opacity: isDisabled ? '0.5' : '1' }}
+        disabled={isDisabled}
+      >
+        Search Photos
+      </button>
     </section>
   )
 }
