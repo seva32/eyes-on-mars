29009c279174f8a89725dcf3ecbcef0022651ce0
Sebs <seva32@yahoo.es>
2025-03-25 12:39:37 -0400

    fix background resize

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   const currentYear = new Date().getFullYear()
 
   return (
-    <div className="bg-[url('/images/mars.jpg')] bg-cover bg-center flex flex-col">
+    <div className="min-h-screen flex flex-col bg-[url('/images/mars.jpg')] bg-cover bg-center">
       <NavBar />
       <main className="flex-1 flex w-full p-4">{children}</main>
       <footer className="p-4 text-orange-300 text-center text-xs font-bold">
