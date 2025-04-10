8e93918194ae5e247354b5a3041f998dbb320c9e
seva32 <seva32@yahoo.es>
2025-04-10 13:02:41 -0400

    update docker cypress

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,6 @@
 Cypress.Commands.add('btnClick', (buttonTestId) => {
   cy.get(`[data-testid="${buttonTestId}"]`).click()
 })
+Cypress.Commands.add('btnGetByText', (buttonText) => {
+  cy.contains('button', buttonText).scrollIntoView().should('be.visible')
+})
